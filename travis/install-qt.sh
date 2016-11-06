01a18d2fc4b609400fd2e9f19da7e352edc5fb25
Christian Juth <cjuth2@gmail.com>
2016-11-06 16:32:37 -0500

    another try

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -2,5 +2,5 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install qt
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get update
-  sudo apt-get install qt-sdk
+  sudo apt-get install libqt4pas
 fi
