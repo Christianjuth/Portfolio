d57d6d70c93984332452e2f8986c1e435534007f
Christian Juth <cjuth2@gmail.com>
2016-11-06 16:19:19 -0500

    trying to fix ubuntu

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -2,5 +2,5 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install qt
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get update
-  sudo apt-get install qt4-qmake libqt4-dev
+  sudo apt-get install qt-sdk
 fi
