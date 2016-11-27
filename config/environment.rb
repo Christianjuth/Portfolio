4fda277e2b765f80c99c74d560018dc43f808481
Christian Juth <cjuth2@gmail.com>
2016-11-27 16:09:21 -0500

    fixed header image size and coverage

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,18 @@ end
 configure :test do
   set :db_adapter, "sqlite3"
   set :db_location, "db/cucumber-test.db"
-  SimpleCov.start
+  
+  SimpleCov.start do
+    add_filter "db/"
+    add_filter "features/"
+    add_filter "travis/"
+    add_filter "node_modules/"
+    add_filter "public/bower/"
+    
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Views', 'app/views'
+  end
   if File.exist?(".github-token")
     Codacy::Reporter.start
   end
