5c5f3ae642c5d29decbb1051120b45fc79fd20a8
Christian Juth <cjuth2@gmail.com>
2017-01-30 08:16:49 -0500

    fix travis

---
+++ travis/fix-brew.sh
@@ -0,0 +1,4 @@
+export LDFLAGS=-L/usr/local/opt/openssl/lib
+export CPPFLAGS=-I/usr/local/opt/openssl/include
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+rvm install $(ruby -v) --autolibs=homebrew
