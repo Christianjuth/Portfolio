84e670771744d6430e05e8464688bb876124e648
Christian Juth <cjuth2@gmail.com>
2017-02-02 13:23:13 -0500

    fixed recaptcha

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -32,6 +32,8 @@ module Helpers
   def recaptcha
     if Sinatra::Application.test? || (ApiVerification.exists?({name: "recaptcha"}) && verify_recaptcha)
       yield
+    else if !ApiVerification.exists?({name: "recaptcha"})
+      yield
     else
       return_request(false, request.referer, "Recaptcha failed")
     end
