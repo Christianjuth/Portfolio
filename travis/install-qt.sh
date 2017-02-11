5b544b0434c3b772dff1d48b2f43ac2c5b161294
Christian Juth <cjuth2@gmail.com>
2017-02-11 17:09:22 -0500

    try qt5

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -2,8 +2,8 @@
 
 if [ "$(uname)" == "Darwin" ]; then
   brew tap homebrew/versions
-  brew install qt
-  brew link --force qt
+  brew install qt55
+  brew link --force qt55
 elif [ "$(uname)" == "Linux" ]; then
   sudo apt-get install libqt4-dev libqtwebkit-dev
 fi
