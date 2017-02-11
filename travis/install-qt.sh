234b214b6be43836887a43a1b9a7d4892c493721
Christian Juth <cjuth2@gmail.com>
2017-02-11 16:55:47 -0500

    still fixing install qt

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
-  echo 'export PATH="/usr/local/opt/qt5/bin:$PATH"' >> ~/.bash_profile
+  brew tap homebrew/versions
   brew install qt
+  brew link --force qt
 elif [ "$(uname)" == "Linux" ]; then
   sudo apt-get install libqt4-dev libqtwebkit-dev
 fi
