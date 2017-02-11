642aec51d039bc94da08280b32d5bfaecc532469
Christian Juth <cjuth2@gmail.com>
2017-02-11 16:38:09 -0500

    fix qmake

--- travis/install-qt.sh
+++ travis/install-qt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
+  echo 'export PATH="/usr/local/opt/qt5/bin:$PATH"' >> ~/.bash_profile
   brew install qt
 elif [ "$(uname)" == "Linux" ]; then
   sudo apt-get install libqt4-dev libqtwebkit-dev
