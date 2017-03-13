ef84c1b23f0fd61bc04637525045dd17b21113d2
Christian Juth <cjuth2@gmail.com>
2017-03-12 22:30:34 -0400

    fix deploy

--- travis/install-qt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(uname)" == "Darwin" ]; then
-  brew tap homebrew/versions
-  brew install qt55
-  brew link --force qt55
-elif [ "$(uname)" == "Linux" ]; then
-  sudo apt-get install libqt4-dev libqtwebkit-dev
-fi
